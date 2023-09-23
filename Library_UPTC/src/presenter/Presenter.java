a74f3b272c7df2cb78b0b46e3c48c592c8d3a8bb
SebastianZarate <juzaor2003@gmail.com>
2023-09-23 16:27:36 -0500

    Se añadió el Dashboard principal

--- Library_UPTC/src/presenter/Presenter.java
+++ Library_UPTC/src/presenter/Presenter.java
@@ -1,4 +1,22 @@
 package presenter;
 
+import view.Dashboard;
+
 public class Presenter {
+
+    Dashboard dashboard;
+
+    public Presenter() {
+        dashboard = new Dashboard();
+    }
+
+    public static void main(String[] args) {
+        try {
+            javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        new Presenter();
+
+    }
 }
--- Library_UPTC/src/view/Dashboard.java
+++ Library_UPTC/src/view/Dashboard.java
@@ -1,4 +1,17 @@
 package view;
 
-public class Dashboard {
+import javax.swing.*;
+import java.awt.*;
+
+public class Dashboard extends JFrame {
+
+    private Toolkit toolkit;
+    private Dimension dimension;
+    public Dashboard(){
+        toolkit = Toolkit.getDefaultToolkit();
+        dimension = toolkit.getScreenSize();
+        setSize(dimension);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
